d7729ef371358c14436292a33b9808b3208be37d
Srinivas <kurumallasrinivas123@gmail.com>
2025-07-12 21:17:55 +0530

    findPeak

--- BinarySearch/findPeakElement.js
+++ BinarySearch/findPeakElement.js
@@ -3,18 +3,18 @@
  * @return {number}
  */
 var findPeakElement = function (nums) {
+  let l = 0;
+  let r = nums.length - 1;
 
-    let l = 0;
-    let r = nums.length - 1;
-
-    while (l < r) {
-        let m = l + Math.floor((r - l) / 2);
-        if (nums[m] > nums[m + 1]) {
-            r = m;
-        }
-        else {
-            l = m + 1;
-        }
+  while (l < r) {
+    let m = l + Math.floor((r - l) / 2);
+    if (nums[m] > nums[m + 1]) {
+      // drawn graph on book, peak must be m or before that
+      r = m;
+    } else {
+      //here m can't be peak for sure, so excluding m
+      l = m + 1;
     }
-    return r;
+  }
+  return r;
 };
